b7af62ffab7b0f6bece8d4683fde1f761f76e7e9
EricLeeN1 <179310009@qq.com>
2023-02-13 20:49:54 +0800

    first commit

---
+++ clone.js
@@ -0,0 +1,28 @@
+export function type(data) {
+  return Object.prototype.toString.call(data).slice(8, -1).toLowerCase();
+}
+
+console.log(type([]));
+console.log(type({}));
+
+export function clone(source) {
+  const t = type(source);
+  if (t !== "object" && t !== "array") {
+    return source;
+  }
+  let target;
+  if (t === "object") {
+    target = {};
+    for (const i in source) {
+      if (Object.hasOwnProperty(i)) {
+        target[i] = clone(source[i]);
+      }
+    }
+  } else {
+    target = [];
+    for (let i = 0; i < array.length; i++) {
+      target[i] = clone(source[i]);
+    }
+  }
+  return target;
+}
